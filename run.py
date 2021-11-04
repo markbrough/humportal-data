980468ebb96792ca3705dab59d7c9aa35a230476
Mark Brough <mark.brough@gmail.com>
2021-11-04 13:43:17 +0000

    Fixes to use same data source for consistency

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ HUMANITARIAN_ANALYTICS = BASE_URL.format("humanitarian.csv")
 VERSIONS = STATS_URL.format("current/inverted-publisher/versions.json")
 CODELIST_VALUES = STATS_URL.format("current/inverted-publisher/codelist_values.json")
 ELEMENTS = STATS_URL.format("current/inverted-publisher/elements.json")
+ACTIVITIES = STATS_URL.format("current/inverted-publisher/activities.json")
+HUMANITARIAN = STATS_URL.format("current/inverted-publisher/humanitarian.json")
 FREQUENCY = BASE_URL.format("timeliness_frequency.csv")
 
 def get_source_data():
@@ -43,6 +45,14 @@ def get_source_data():
     with open("cache/elements.json", 'w') as elements_json:
         elements_json.write(r.text)
 
+    r = requests.get(ACTIVITIES)
+    with open("cache/activities.json", 'w') as activities_json:
+        activities_json.write(r.text)
+
+    r = requests.get(HUMANITARIAN)
+    with open("cache/humanitarian.json", 'w') as humanitarian_json:
+        humanitarian_json.write(r.text)
+
 
 def generate_homepage_stats(analytics_publishers):
     signatories = set()
@@ -56,10 +66,11 @@ def generate_homepage_stats(analytics_publishers):
                     'iati': False,
                     'humanitarian': False
                 }
-            if row['Registred Pub. ID'] in analytics_publishers:
-                if int(analytics_publishers[row['Registred Pub. ID']]['Number of Activities']) > 0:
+            publisher_id = row['Registred Pub. ID']
+            if publisher_id in analytics_publishers:
+                if int(analytics_publishers[publisher_id]['Number of Activities']) > 0:
                     signatories_data[row['GB signatory']]['iati'] = True
-                if int(analytics_publishers[row['Registred Pub. ID']]['Publishing Humanitarian']) > 0:
+                if int(analytics_publishers[publisher_id]['Publishing Humanitarian']) > 0:
                     signatories_data[row['GB signatory']]['humanitarian'] = True
 
     with open('output/homepage.json', 'w') as jsonfile:
@@ -83,37 +94,45 @@ def generate_signatory_data(analytics_publishers):
         elements = json.load(elements_json)
     with open('cache/frequency.json', 'r') as frequency_json:
         frequency = json.load(frequency_json)
+    with open('cache/activities.json', 'r') as activities_json:
+        activities = json.load(activities_json)
+    with open('cache/humanitarian.json', 'r') as humanitarian_json:
+        humanitarian = json.load(humanitarian_json)
     with open('data/signatories.csv', 'r') as signatories_csv:
         csvreader = csv.DictReader(signatories_csv)
         for row in csvreader:
-            pub = analytics_publishers.get(row['Registred Pub. ID'], {})
+            publisher_id = row['Registred Pub. ID']
+            pub = analytics_publishers.get(publisher_id, {})
             hum_data = float(pub.get('Publishing Humanitarian', 0)) > 0
-            _202_hum_data = (float(pub.get('Using Humanitarian Attribute', 0)) > 0) or \
-                (float(pub.get('Appeal or Emergency Details', 0)) > 0) or \
-                (float(pub.get('Clusters', 0)) > 0)
-            _203_hum_data = ((row['Registred Pub. ID'] in codelist_values['.//transaction/transaction-type/@code']['12'].keys()) or
-                (row['Registred Pub. ID'] in codelist_values['.//transaction/transaction-type/@code']['13'].keys()) or
-                (row['Registred Pub. ID'] in codelist_values['.//default-aid-type/@vocabulary']['2'].keys()) or
-                (row['Registred Pub. ID'] in codelist_values['.//default-aid-type/@vocabulary']['3'].keys()) or
-                (row['Registred Pub. ID'] in codelist_values['.//participating-org/@type']['24'].keys()) or
-                (row['Registred Pub. ID'] in codelist_values['.//transaction/provider-org/@type']['24'].keys()) or
-                (row['Registred Pub. ID'] in codelist_values['.//transaction/receiver-org/@type']['24'].keys())
+            num_activities = activities.get(publisher_id, 0)
+            num_humanitarian_activities = humanitarian['is_humanitarian'].get(publisher_id, 0)
+            _202_hum_data = (humanitarian['is_humanitarian_by_attrib'].get(publisher_id, 0) > 0) or \
+                (humanitarian['contains_humanitarian_scope_without_humanitarian'].get(publisher_id, 0) > 0)
+            _203_hum_data = ((publisher_id in codelist_values['.//transaction/transaction-type/@code']['12'].keys()) or
+                (publisher_id in codelist_values['.//transaction/transaction-type/@code']['13'].keys()) or
+                (publisher_id in codelist_values['.//default-aid-type/@vocabulary']['2'].keys()) or
+                (publisher_id in codelist_values['.//default-aid-type/@vocabulary']['3'].keys()) or
+                (publisher_id in codelist_values['.//participating-org/@type']['24'].keys()) or
+                (publisher_id in codelist_values['.//transaction/provider-org/@type']['24'].keys()) or
+                (publisher_id in codelist_values['.//transaction/receiver-org/@type']['24'].keys())
                 )
-            monthly = frequency.get(row['Registred Pub. ID']) == "Monthly"
-            traceability = row['Registred Pub. ID'] in elements['iati-activity/transaction/provider-org/@provider-activity-id'].keys()
+            monthly = frequency.get(publisher_id) == "Monthly"
+            traceability = publisher_id in elements['iati-activity/transaction/provider-org/@provider-activity-id'].keys()
             publishers.append({
-                'publisherID': row['Registred Pub. ID'],
+                'publisherID': publisher_id,
                 'iatiOrganisationID': row['IATI organisation reference'],
                 'name': row['Publisher'],
                 'gbSignatory': row['GB signatory'],
                 'organisationType': row['Organisation type'],
-                'iatiVersion': versions.get(row['Registred Pub. ID'], None),
+                'iatiVersion': versions.get(publisher_id, None),
                 'humData': hum_data,
+                'humanitarianActivities': num_humanitarian_activities,
+                'activities': num_activities,
                 '202HumData': _202_hum_data,
                 '203HumData': _203_hum_data,
                 'traceability': traceability,
                 'monthly': monthly,
-                'frequency': frequency.get(row['Registred Pub. ID'])
+                'frequency': frequency.get(publisher_id)
             })
     with open('output/signatories.json', 'w') as jsonfile:
         json.dump(publishers, jsonfile)
@@ -123,8 +142,6 @@ def generate_signatories_progress():
     with open('output/homepage.json', 'r') as homepage_stats_jsonfile:
         homepage_stats = json.load(homepage_stats_jsonfile)
         num_signatories = homepage_stats['signatories']
-        num_iati = homepage_stats['iati']
-        num_hum = homepage_stats['humanitarian']
 
     signatories = {}
     with open('output/signatories.json', 'r') as signatories_json_file:
@@ -135,7 +152,9 @@ def generate_signatories_progress():
                     '202': False,
                     'granular_202': False,
                     'granular_203': False,
-                    'traceability': False
+                    'traceability': False,
+                    'iati': False,
+                    'humanitarian': False
                 }
             if sig['iatiVersion'] in ['2.02', '2.03']:
                 signatories[sig['gbSignatory']]['202'] = True
@@ -145,11 +164,17 @@ def generate_signatories_progress():
                 signatories[sig['gbSignatory']]['granular_203'] = True
             if sig['traceability'] == True:
                 signatories[sig['gbSignatory']]['traceability'] = True
+            if sig['activities'] > 0:
+                signatories[sig['gbSignatory']]['iati'] = True
+            if sig['humanitarianActivities'] > 0:
+                signatories[sig['gbSignatory']]['humanitarian'] = True
 
     num_202 = len(list(filter(lambda signatory: signatory['202'] == True, signatories.values())))
     num_granular_202 = len(list(filter(lambda signatory: signatory['granular_202'] == True, signatories.values())))
     num_granular_203 = len(list(filter(lambda signatory: signatory['granular_203'] == True, signatories.values())))
     num_traceability = len(list(filter(lambda signatory: signatory['traceability'] == True, signatories.values())))
+    num_iati = len(list(filter(lambda signatory: signatory['iati'] == True, signatories.values())))
+    num_hum = len(list(filter(lambda signatory: signatory['humanitarian'] == True, signatories.values())))
 
     today = {
         'id': '',
